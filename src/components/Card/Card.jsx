e4017d58a2777680df21f28a20beb298e5b46976
Sirgans <102359564+Sirgans@users.noreply.github.com>
2022-07-28 18:46:37 +0400

    add mockapi

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,27 +1,29 @@
 import React from 'react';
-import image from '../../utils/assets/product/blackberry.png'
 import './Card.css'
 
-const Card = () => {
+
+
+const Card = ({image, title, subtitle, value}) => {
     return (
         <article className='card'> 
             <div className='card__image-wrapper'>
-                <img className='card__image' src={image} alt="Изображение напитка"/>  
+                <img className='card__image' src={require(`../../assets/product/${image}`)} alt="Изображение напитка"/>  
             </div>                                                  
             <div className='card__name'>
                 <h3 className='card__title'>
-                    Blackberry
+                    {title}
                 </h3>
                 <h4 className='card__subtitle'>
-                    Освежающий напиток
+                    {subtitle}
                 </h4>
             </div>  
             <div className='card__information'>
-                <p className='card__information-value'>30%</p>
+                <p className='card__information-value'>{value}</p>
                 <p className='card__information-title'>Алкоголь</p>
             </div>          
         </article>
     )
 }
+
 
 export default Card
