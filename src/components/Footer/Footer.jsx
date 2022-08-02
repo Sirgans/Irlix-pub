b9393156e7ba41393446c7c1b893b9e2e8e769bd
Сергей Софронов <sergei.sofronov@irlix.ru>
2022-08-02 20:11:02 +0400

    gh-page

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, {useContext, useState} from "react";
+import { Catigory } from "../../App";
 import './Footer.css'
 
+
 const Footer = () => {
+    const [state, setState] = useState('')
+    const {setSearchValue} = useContext(Catigory)
+    const setValue = (value) => {
+        setState(value)
+        setSearchValue(value) 
+    }
     return (
         <footer className="footer">
             <div className="container">
-                <button className="button">Поиск</button>
+                <input className="search_input" value = {state} placeholder = 'Поиск' onChange={e => setValue(e.target.value)}/>
             </div>         
         </footer>
     )
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,27 +1,32 @@
-import React, {useEffect, useState} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import Card from './../Card/Card';
 import '../Card/Card.css'
 import axios from 'axios'
-
+import { Catigory } from '../../App';
 
 const url = 'https://62debc5a9c47ff309e7ae90e.mockapi.io/Cards'
 
     
 const Main = () => {
     const [card, setCard] = useState([])
+    const {setContext, selectCatigory, searchValue} = useContext(Catigory)
+    
     useEffect(() => {
         axios.get(url).then((response) => {
             setCard(response.data)
+            setContext(response.data)
         })
     }, [])
-    
-    if (!card) return null
+    console.log(card)
     
     return (
         <main className="main">
             <div className="container">   
                 <section className='cards'>
-                    {card.map(card =>{
+                    {!card.length && <div style={{color: 'white'}}>'Идет загрузка'</div>}
+                    {card.filter(item => item.filter.includes(selectCatigory))
+                            .filter(item => item.title.toLowerCase().includes(searchValue.toLowerCase()))
+                            .map(card =>{
                         return (
                       <Card key={card.id} title={card.title} image={card.image} subtitle={card.subtitle} value={card.value} />  
                         )
