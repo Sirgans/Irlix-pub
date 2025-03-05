d80b9accf624855db8c5288d009e86ce97880cf0
Sirgans <102359564+Sirgans@users.noreply.github.com>
2025-03-05 13:20:50 +0400

    Update pub.api.js

--- src/store/pub/pub.api.js
+++ src/store/pub/pub.api.js
@@ -6,14 +6,14 @@ export const api = {
     getAllCards: createAsyncThunk(
         'pub/getAll',
         async function () {
-            const { data } = await axios.get('https://62debc5a9c47ff309e7ae90e.mockapi.io/Cards')
+            const { data } = await axios.get('https://62debc5a9c47ff309e7ae90e.mockapi.io/a')
             return data
         }
     ),
     getOneCard: createAsyncThunk(
         'pub/getOne',
         async (id) => {
-        const { data } = await axios.get(`https://62debc5a9c47ff309e7ae90e.mockapi.io/Cards/${id}`)
+        const { data } = await axios.get(`https://62debc5a9c47ff309e7ae90e.mockapi.io/a/${id}`)
         return data
     })
 }
